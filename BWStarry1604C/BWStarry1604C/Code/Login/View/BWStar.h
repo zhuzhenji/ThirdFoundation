74f08ebfeb1f3f64e81606b98d23dacb57123d46
zhuzhenji <1418658460@qq.com>
2018-07-05 16:47:39 +0800

    buchond
    001

---
+++ BWStarry1604C/BWStarry1604C/Code/Login/View/BWStar.h
@@ -0,0 +1,13 @@
+//
+//  BWStar.h
+//  BWStarry1604C
+//
+//  Created by 朱振吉 on 2018/7/5.
+//  Copyright © 2018年 朱振吉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWStar : UIView
+
+@end
