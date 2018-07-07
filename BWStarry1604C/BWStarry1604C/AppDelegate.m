6671e0e8c762660f820d0595af71433624870370
zhuzhenji <1418658460@qq.com>
2018-07-07 12:04:44 +0800

    报错
    报错

--- BWStarry1604C/BWStarry1604C/AppDelegate.m
+++ BWStarry1604C/BWStarry1604C/AppDelegate.m
@@ -9,7 +9,6 @@
 #import "AppDelegate.h"
 #import "LoginViewController.h"
 #import <UMCommon/UMCommon.h>
-#import <>
 @interface AppDelegate ()
 
 @end
@@ -22,137 +21,84 @@
     self.window.rootViewController=[LoginViewController new];
     //初始化友盟sdk
     [UMConfigure initWithAppkey:@"5b3de763a40fa319900000d7" channel:@"App Store"];
-    
-    
-    // Push组件基本功能配置
-    UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
-    //type是对推送的几个参数的选择，可以选择一个或者多个。默认是三个全部打开，即：声音，弹窗，角标
-    entity.types = UMessageAuthorizationOptionBadge|UMessageAuthorizationOptionSound|UMessageAuthorizationOptionAlert;
-    [UNUserNotificationCenter currentNotificationCenter].delegate=self;
-    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity     completionHandler:^(BOOL granted, NSError * _Nullable error) {
-        if (granted) {
-        }else{
-        }
-    }];
-    
-    
-    // Push功能配置
-    UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
-    entity.types = UMessageAuthorizationOptionBadge|UMessageAuthorizationOptionAlert|UMessageAuthorizationOptionSound;
-    //如果你期望使用交互式(只有iOS 8.0及以上有)的通知，请参考下面注释部分的初始化代码
-    if (([[[UIDevice currentDevice] systemVersion]intValue]>=8)&&([[[UIDevice currentDevice] systemVersion]intValue]<10)) {
-        UIMutableUserNotificationAction *action1 = [[UIMutableUserNotificationAction alloc] init];
-        action1.identifier = @"action1_identifier";
-        action1.title=@"打开应用";
-        action1.activationMode = UIUserNotificationActivationModeForeground;//当点击的时候启动程序
-        
-        UIMutableUserNotificationAction *action2 = [[UIMutableUserNotificationAction alloc] init];  //第二按钮
-        action2.identifier = @"action2_identifier";
-        action2.title=@"忽略";
-        action2.activationMode = UIUserNotificationActivationModeBackground;//当点击的时候不启动程序，在后台处理
-        action2.authenticationRequired = YES;//需要解锁才能处理，如果action.activationMode = UIUserNotificationActivationModeForeground;则这个属性被忽略；
-        action2.destructive = YES;
-        UIMutableUserNotificationCategory *actionCategory1 = [[UIMutableUserNotificationCategory alloc] init];
-        actionCategory1.identifier = @"category1";//这组动作的唯一标示
-        [actionCategory1 setActions:@[action1,action2] forContext:(UIUserNotificationActionContextDefault)];
-        NSSet *categories = [NSSet setWithObjects:actionCategory1, nil];
-        entity.categories=categories;
-    }
-    //如果要在iOS10显示交互式的通知，必须注意实现以下代码
-    if ([[[UIDevice currentDevice] systemVersion]intValue]>=10) {
-        UNNotificationAction *action1_ios10 = [UNNotificationAction actionWithIdentifier:@"action1_identifier" title:@"打开应用" options:UNNotificationActionOptionForeground];
-        UNNotificationAction *action2_ios10 = [UNNotificationAction actionWithIdentifier:@"action2_identifier" title:@"忽略" options:UNNotificationActionOptionForeground];
-        
-        //UNNotificationCategoryOptionNone
-        //UNNotificationCategoryOptionCustomDismissAction  清除通知被触发会走通知的代理方法
-        //UNNotificationCategoryOptionAllowInCarPlay       适用于行车模式
-        UNNotificationCategory *category1_ios10 = [UNNotificationCategory categoryWithIdentifier:@"category1" actions:@[action1_ios10,action2_ios10]   intentIdentifiers:@[] options:UNNotificationCategoryOptionCustomDismissAction];
-        NSSet *categories = [NSSet setWithObjects:category1_ios10, nil];
-        entity.categories=categories;
-    }
-    [UNUserNotificationCenter currentNotificationCenter].delegate=self;
-    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity completionHandler:^(BOOL granted, NSError * _Nullable error) {
-        if (granted) {
-        }else{
-        }
-    }];
+
     
     
     return YES;
 }
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    // Saves changes in the application's managed object context before the application terminates.
-    [self saveContext];
-}
-
-
-#pragma mark - Core Data stack
-
-@synthesize persistentContainer = _persistentContainer;
-
-- (NSPersistentContainer *)persistentContainer {
-    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
-    @synchronized (self) {
-        if (_persistentContainer == nil) {
-            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"BWStarry1604C"];
-            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
-                if (error != nil) {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    
-                    /*
-                     Typical reasons for an error here include:
-                     * The parent directory does not exist, cannot be created, or disallows writing.
-                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                     * The device is out of space.
-                     * The store could not be migrated to the current model version.
-                     Check the error message to determine what the actual problem was.
-                    */
-                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-                    abort();
-                }
-            }];
-        }
-    }
-    
-    return _persistentContainer;
-}
-
-#pragma mark - Core Data Saving support
-
-- (void)saveContext {
-    NSManagedObjectContext *context = self.persistentContainer.viewContext;
-    NSError *error = nil;
-    if ([context hasChanges] && ![context save:&error]) {
-        // Replace this implementation with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
-        abort();
-    }
-}
-
+//
+//- (void)applicationWillResignActive:(UIApplication *)application {
+//    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+//    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+//}
+//
+//
+//- (void)applicationDidEnterBackground:(UIApplication *)application {
+//    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+//    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+//}
+//
+//
+//- (void)applicationWillEnterForeground:(UIApplication *)application {
+//    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+//}
+//
+//
+//- (void)applicationDidBecomeActive:(UIApplication *)application {
+//    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+//}
+//
+//
+//- (void)applicationWillTerminate:(UIApplication *)application {
+//    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+//    // Saves changes in the application's managed object context before the application terminates.
+//    [self saveContext];
+//}
+//
+//
+//#pragma mark - Core Data stack
+//
+//@synthesize persistentContainer = _persistentContainer;
+//
+//- (NSPersistentContainer *)persistentContainer {
+//    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+//    @synchronized (self) {
+//        if (_persistentContainer == nil) {
+//            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"BWStarry1604C"];
+//            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
+//                if (error != nil) {
+//                    // Replace this implementation with code to handle the error appropriately.
+//                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//
+//                    /*
+//                     Typical reasons for an error here include:
+//                     * The parent directory does not exist, cannot be created, or disallows writing.
+//                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+//                     * The device is out of space.
+//                     * The store could not be migrated to the current model version.
+//                     Check the error message to determine what the actual problem was.
+//                    */
+//                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+//                    abort();
+//                }
+//            }];
+//        }
+//    }
+//
+//    return _persistentContainer;
+//}
+//
+//#pragma mark - Core Data Saving support
+//
+//- (void)saveContext {
+//    NSManagedObjectContext *context = self.persistentContainer.viewContext;
+//    NSError *error = nil;
+//    if ([context hasChanges] && ![context save:&error]) {
+//        // Replace this implementation with code to handle the error appropriately.
+//        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+//        abort();
+//    }
+//}
+//
 @end
