4f891660217034f4b7c91d820f5ce74444269cac
zhuzhenji <1418658460@qq.com>
2018-07-07 09:44:31 +0800

    tuisong
    youmengtuisong

--- BWStarry1604C/BWStarry1604C/AppDelegate.m
+++ BWStarry1604C/BWStarry1604C/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "LoginViewController.h"
 #import <UMCommon/UMCommon.h>
+#import <>
 @interface AppDelegate ()
 
 @end
@@ -19,7 +20,64 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     //将窗口指向登录界面
     self.window.rootViewController=[LoginViewController new];
+    //初始化友盟sdk
     [UMConfigure initWithAppkey:@"5b3de763a40fa319900000d7" channel:@"App Store"];
+    
+    
+    // Push组件基本功能配置
+    UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
+    //type是对推送的几个参数的选择，可以选择一个或者多个。默认是三个全部打开，即：声音，弹窗，角标
+    entity.types = UMessageAuthorizationOptionBadge|UMessageAuthorizationOptionSound|UMessageAuthorizationOptionAlert;
+    [UNUserNotificationCenter currentNotificationCenter].delegate=self;
+    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity     completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+        }else{
+        }
+    }];
+    
+    
+    // Push功能配置
+    UMessageRegisterEntity * entity = [[UMessageRegisterEntity alloc] init];
+    entity.types = UMessageAuthorizationOptionBadge|UMessageAuthorizationOptionAlert|UMessageAuthorizationOptionSound;
+    //如果你期望使用交互式(只有iOS 8.0及以上有)的通知，请参考下面注释部分的初始化代码
+    if (([[[UIDevice currentDevice] systemVersion]intValue]>=8)&&([[[UIDevice currentDevice] systemVersion]intValue]<10)) {
+        UIMutableUserNotificationAction *action1 = [[UIMutableUserNotificationAction alloc] init];
+        action1.identifier = @"action1_identifier";
+        action1.title=@"打开应用";
+        action1.activationMode = UIUserNotificationActivationModeForeground;//当点击的时候启动程序
+        
+        UIMutableUserNotificationAction *action2 = [[UIMutableUserNotificationAction alloc] init];  //第二按钮
+        action2.identifier = @"action2_identifier";
+        action2.title=@"忽略";
+        action2.activationMode = UIUserNotificationActivationModeBackground;//当点击的时候不启动程序，在后台处理
+        action2.authenticationRequired = YES;//需要解锁才能处理，如果action.activationMode = UIUserNotificationActivationModeForeground;则这个属性被忽略；
+        action2.destructive = YES;
+        UIMutableUserNotificationCategory *actionCategory1 = [[UIMutableUserNotificationCategory alloc] init];
+        actionCategory1.identifier = @"category1";//这组动作的唯一标示
+        [actionCategory1 setActions:@[action1,action2] forContext:(UIUserNotificationActionContextDefault)];
+        NSSet *categories = [NSSet setWithObjects:actionCategory1, nil];
+        entity.categories=categories;
+    }
+    //如果要在iOS10显示交互式的通知，必须注意实现以下代码
+    if ([[[UIDevice currentDevice] systemVersion]intValue]>=10) {
+        UNNotificationAction *action1_ios10 = [UNNotificationAction actionWithIdentifier:@"action1_identifier" title:@"打开应用" options:UNNotificationActionOptionForeground];
+        UNNotificationAction *action2_ios10 = [UNNotificationAction actionWithIdentifier:@"action2_identifier" title:@"忽略" options:UNNotificationActionOptionForeground];
+        
+        //UNNotificationCategoryOptionNone
+        //UNNotificationCategoryOptionCustomDismissAction  清除通知被触发会走通知的代理方法
+        //UNNotificationCategoryOptionAllowInCarPlay       适用于行车模式
+        UNNotificationCategory *category1_ios10 = [UNNotificationCategory categoryWithIdentifier:@"category1" actions:@[action1_ios10,action2_ios10]   intentIdentifiers:@[] options:UNNotificationCategoryOptionCustomDismissAction];
+        NSSet *categories = [NSSet setWithObjects:category1_ios10, nil];
+        entity.categories=categories;
+    }
+    [UNUserNotificationCenter currentNotificationCenter].delegate=self;
+    [UMessage registerForRemoteNotificationsWithLaunchOptions:launchOptions Entity:entity completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+        }else{
+        }
+    }];
+    
+    
     return YES;
 }
 
